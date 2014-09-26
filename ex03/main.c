ce46e62dcb09aeab4ee21b034d7d28d35ff7ec51
renanbatko <renanbatkousp@gmail.com>
2014-09-26 17:53:35 -0300

    continuando

--- ex03/main.c
+++ ex03/main.c
@@ -16,16 +16,35 @@ int main(void){
 	//apagar_item(item);
 	int i, a, b;
 	for (i = 0; i < 4; i++){
-		scanf(" %d %d", &a, &b);
-		//a = i + 2;
-		//b = i + 9;
+		//scanf(" %d %d", &a, &b);
+		a = i + 2;
+		b = i + 9;
 		item = criar_item(a, b);
 		inserir_ordenado(li, item);
 		apagar_item(item);
 	}
 	
+	printf("\n");
+	item = recuperar_item(li, 4);
+	imprimir_item(item);
+	
+	printf("\n");
 	imprimir_lista(li);
 	
+	printf("tamanho: %d\n", tamanho(li));
+	
+	remover_item(li, 2);
+	printf("\n");
+	imprimir_lista(li);
+	
+	remover_item(li, 5);
+	printf("\n");
+	imprimir_lista(li);
+	
+	printf("tamanho: %d\n", tamanho(li));
+
+
+
 	apagar_lista(li);
 	
 	return 0;
